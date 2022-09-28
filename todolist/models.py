528fb8e44377742781e653d1222dde4f560f6469
Bintang Pratama <bintang0102pratama@gmail.com>
2022-09-28 15:48:02 +0700

    fix tkinter import problem

--- todolist/models.py
+++ todolist/models.py
@@ -1,7 +1,7 @@
-from tkinter import CASCADE
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
+# from django.db.models.deletion import CASCADE
 # Create your models here.
 class Task(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
